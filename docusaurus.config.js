550f090b9adfd6112ef77c93d5bce832fcfa7f45
Kor Dwarshuis <proximus@fastmail.com>
2026-05-10 11:59:58 +0200

    Update GitHub pages deployment config with correct organization and project names

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kordwarshuis', // Usually your GitHub org/user name.
+  projectName: 'KERIKOR', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
