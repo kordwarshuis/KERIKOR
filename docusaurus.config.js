dd3516391a3c3b7217f05aef947c0f2158a00f4d
Kor Dwarshuis <proximus@fastmail.com>
2026-05-10 17:29:12 +0200

    fix: update editUrl in Docusaurus config to point to the correct repository

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/kordwarshuis/KERIKOR/tree/main/docs/',
           sidebarCollapsed: false,   // All categories expanded by default
           // sidebarCollapsible: false, // Optional: disable collapsing entirely
         },
